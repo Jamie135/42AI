78524b78eec3ae35d1adf4f86c029a9835f78d2f
Jooma <jamie.gworld@hotmail.com>
2024-06-11 21:58:50 +0200

    Module02/ex00

---
+++ Python/Module02/ex00/ft_map.py
@@ -0,0 +1,13 @@
+def ft_map(function_to_apply, iterable):
+    """Map the function to all elements of the iterable.
+    Args:
+    function_to_apply: a function taking an iterable.
+    iterable: an iterable object (list, tuple, iterator).
+    Return:
+    An iterable.
+    None if the iterable can not be used by the function.
+    """
+    res = []
+    for i in iterable:
+        res.append(function_to_apply(i))
+    return res
---
+++ Python/Module02/ex00/ft_reduce.py
@@ -0,0 +1,15 @@
+def ft_reduce(function_to_apply, iterable):
+    """Apply function of two arguments cumulatively.
+    Args:
+    function_to_apply: a function taking an iterable.
+    iterable: an iterable object (list, tuple, iterator).
+    Return:
+    A value, of same type of elements in the iterable parameter.
+    None if the iterable can not be used by the function.
+    """
+
+    # an iterable object has an __iter__ attribute
+    if not hasattr(iterable, '__iter__'):
+        raise TypeError("Passed object must be iterable")
+    for i in iterable:
+        yield function_to_apply(i)
